ba4489f20599be4eb6c678cf4534ba80f7bb50f1
Jake <j.mcmillan8@rgu.ac.uk>
2025-02-20 16:36:04 +0000

    Lab 4 Complete

--- Assets/Scripts/HMD_Manager.cs
+++ Assets/Scripts/HMD_Manager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class HMD_Manager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class HMD_Manager : MonoBehaviour{
+    [SerializeField] GameObject xrPlayer;
+    [SerializeField] GameObject fpsPlayer;
+
+    void Start(){
+        Debug.Log("HMD: " + XRSettings.loadedDeviceName);
+        if (XRSettings.isDeviceActive/* || XRSettings.loadedDeviceName == */){
+            fpsPlayer.SetActive(false);
+            xrPlayer.SetActive(true);
+
+        } else {
+            Debug.Log("No HMD Detected, using FPS player");
+            fpsPlayer.SetActive(true);
+            xrPlayer.SetActive(false);
+            
+
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
 }
